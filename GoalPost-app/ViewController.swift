96bffcf81aff1f769cd0503783bc31dfc116c135
LCN1993 <snilove@hotmail.com>
2019-01-29 09:38:25 +0800

    Initial Commit

---
+++ GoalPost-app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GoalPost-app
+//
+//  Created by Ethan  on 29/1/19.
+//  Copyright © 2019 Ethan . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
